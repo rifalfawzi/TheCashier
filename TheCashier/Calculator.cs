53824a433cbdde56c4f6886c0d56f2004f81e2f4
rifalfawzi <rifal.fawzi@students.amikom.ac.id>
2020-10-25 23:23:21 +0700

    TheCashier

---
+++ TheCashier/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCashier
+{
+    class Calculator
+    {
+        private List<Item> ListItem;
+        private double total = 0;
+
+        public Calculator()
+        {
+            this.ListItem = new List<Item>();
+        }
+        public void AddItem(Item item)
+        {
+            this.ListItem.Add(item);
+            this.total += item.getSubTotal();
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+        public List<Item> getListItem()
+        {
+            return ListItem;
+        }
+    }
+}
